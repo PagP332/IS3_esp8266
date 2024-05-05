fce849dddfddea30c5060b8b0e897450eae4ca16
PagP332 <120449335+PagP332@users.noreply.github.com>
2024-05-05 16:46:52 +0800

    Update ldr.ino

--- ldr.ino
+++ ldr.ino
@@ -3,10 +3,12 @@
 Servo UDservo;  // create servo object to control a servo
 Servo LRservo;
 
+#define LDR1 A2 // Up
 #define LDR2 A3 // Right
+#define LDR3 A4 // Down
 #define LDR4 A5 // Left
 
-bool ldr2, ldr4;
+bool ldr1, ldr2, ldr3, ldr4;
 int LRpos, UDpos;
 
 void setup() {
@@ -14,6 +16,8 @@ void setup() {
   Serial.print("Recalibrating... Begin");
   pinMode(LDR2, INPUT);
   pinMode(LDR4, INPUT);
+  pinMode(LDR1, INPUT);
+  pinMode(LDR3, INPUT);
   UDservo.attach(10);
   LRservo.attach(9);
   UDservo.write(90);
@@ -41,6 +45,20 @@ void loop() {
       LRpos -= step_size;
       Serial.print("\tMoving left " + String(LRpos));
       LRservo.write(LRpos);
+    }
+  }
+  if(ldr1 < ldr3){
+    if(LRpos > 45){
+      UDpos += step_size;
+      Serial.print("\tMoving up " + String(LRpos));
+      UDservo.write(UDpos);
+    }
+  }
+  if(ldr1 > ldr3){
+    if(LRpos > 135){
+      UDpos -= step_size;
+      Serial.print("\tMoving down " + String(LRpos));
+      UDservo.write(UDpos);
     }
   }
   Serial.print("\n");
